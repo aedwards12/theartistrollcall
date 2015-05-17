a29c9c58cd1299df901414b50f9c6450e002ae11
Anthony Edwards Jr <anthony.edwardsjr@gmail.com>
2015-05-16 22:56:36 -0400

    added artist search

--- theartistrollcall/app/controllers/welcome_controller.rb
+++ theartistrollcall/app/controllers/welcome_controller.rb
@@ -16,21 +16,28 @@ class WelcomeController < ApplicationController
 
    def search
     # Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-    access_token = prepare_access_token(ENV["ACCESS_TOKEN"], ENV["ACCESS_TOKEN_SECRET"])
+    # access_token = prepare_access_token(ENV["ACCESS_TOKEN"], ENV["ACCESS_TOKEN_SECRET"])
 
-    # use the access token as an agent to get the home timeline
-    response = access_token.request(:get, "https://api.twitter.com/1.1/users/search.json?q=#{params["q"]}&count=5")
-    data = JSON.parse(response.body)
-    data_array = []
-    data.count.times do |x|
-      data_array << {"id"=> x, "text"=> data[x]["screen_name"]}
-    end
+    # # use the access token as an agent to get the home timeline
+    # response = access_token.request(:get, "https://api.twitter.com/1.1/users/search.json?q=#{params["q"]}&count=5")
+    # data = JSON.parse(response.body)
+    # data_array = []
+    # data.count.times do |x|
+    #   data_array << {"id"=> x, "text"=> data[x]["screen_name"]}
+    # end
+    # respond_to do |format|
+    #   format.json { render json: data_array }
+    # end
+    # binding.pry
+
+    @artists = Artist.where('twitter_screen_name ILIKE ?', "%#{params["q"]}%" ).select(['id, twitter_screen_name As text'])
+    logger.ap @artists
     respond_to do |format|
-      format.json { render json: data_array }
+      format.json { render json: @artists}
     end
   end
 
   def twitter_callback_url
-    binding.pry
+    # binding.pry
   end
 end
--- theartistrollcall/app/models/artist.rb
+++ theartistrollcall/app/models/artist.rb
@@ -2,7 +2,9 @@ class Artist < ActiveRecord::Base
   has_many :artist_videos
   has_many :videos, through: :artist_videos
 
-   validates_uniqueness_of :twitter_id
+  validates_uniqueness_of :twitter_id
 
-   # delegate :artist_role, to: :artist_videos
+  def type
+    self.class.name
+  end
 end
