f46431edb79bab0dd91acf931961355473a9cb1c
Anthony Edwards Jr <anthony.edwardsjr@gmail.com>
2015-06-02 22:59:07 -0400

    added video scopes, and artist video sections

--- theartistrollcall/app/controllers/artists_controller.rb
+++ theartistrollcall/app/controllers/artists_controller.rb
@@ -7,6 +7,10 @@ class ArtistsController < ApplicationController
   def load_artist
     @artist =  Artist.find(params[:id])
     @videos = @artist.videos
+
+    @dancer_videos = @artist.videos.merge(ArtistVideo.dancer)
+    @choreography_videos = @artist.videos.merge(ArtistVideo.choreography)
+    @asst_choreography = @artist.videos.merge(ArtistVideo.asst_choreography)
     @recommended_artists = []
     @videos.each do |vid|
       @recommended_artists << vid.artists
--- theartistrollcall/app/models/artist_video.rb
+++ theartistrollcall/app/models/artist_video.rb
@@ -2,6 +2,11 @@ class ArtistVideo < ActiveRecord::Base
   belongs_to :video
   belongs_to :artist
 
+  scope :dancer, ->{where(artist_role: "0")}
+  scope :choreography, ->{where(artist_role: "1")}
+  scope :asst_choreography, ->{where(artist_role: "2")}
+
+
   enum artist_role: [ :dancer, :choreographer, :assist_choreographer ]
 
   def artist
