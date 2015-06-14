ee9edb1a13b4ca5738145fd2933edfa7482d57dd
Anthony Edwards Jr <anthony.edwardsjr@gmail.com>
2015-06-14 00:57:01 -0400

    added mail_form gem, contact form is working with flash message

--- theartistrollcall/config/routes.rb
+++ theartistrollcall/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact_form/new'
+
+  get 'contact_form/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +17,8 @@ Rails.application.routes.draw do
   end
 
   resources :artists
+
+  resources :contact_forms
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
