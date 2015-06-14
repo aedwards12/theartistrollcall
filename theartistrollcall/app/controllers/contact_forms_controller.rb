80e6d3c650e35bc987049ad61b8418f6d54d3ec5
Anthony Edwards Jr <anthony.edwardsjr@gmail.com>
2015-06-14 01:06:44 -0400

    added script to clear contact form

--- theartistrollcall/app/controllers/contact_forms_controller.rb
+++ theartistrollcall/app/controllers/contact_forms_controller.rb
@@ -4,8 +4,10 @@ class ContactFormsController < ApplicationController
 
   def create
     contact_form = ContactForm.new(contact_form_params)
-    if contact_form.deliver
+    if @contact_form = contact_form.deliver
       flash[:success] = "Email has been successfully sent"
+    else
+      flash[:notice] = "Email has not successfully sent"
     end
     render 'welcome/contact_form.js'
   end
